06c504dd4ee15500f8a6bb673cd867b9e0af9861
Lela810 <leandro.klaus03@gmail.com>
2021-10-21 21:37:17 +0200

    Update

---
+++ GIBZMensaAPI/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const { writeFileSync, statSync, readFileSync } = require('fs')
+const moment = require('moment')
+const { join } = require('path')
+const app = express();
+const splitToMenus = require('./index.js')
+
+
+const menuTypes = [
+    "Tagesmenü",
+    "Vegimenü",
+    "Hit",
+    "Topping des Tages"
+]
+
+const archivePath = join(__dirname, '/archive')
+
+app.get('/api/v1/', (req, res) => {
+    const date = req.query.date;
+    const filePath = `${join(archivePath, date)}.json`
+    const url = 'https://zfv.ch/de/microsites/restaurant-treff/menuplan#' + date; //Date must be in format -> 2021-10-22
+    console.log('Date "' + date + '" requested')
+
+    // Decide if menu is already in the archive
+    const dayOfTheWeek = moment(date).format('d')
+    if (!["6", "0"].includes(dayOfTheWeek)) {
+        try {
+            statSync(filePath)
+            const cachedMenu = JSON.parse(readFileSync(filePath, 'utf-8'))
+            res.status(200).send(cachedMenu)
+        } catch (err) {
+            request(url, (error, response, html) => {
+                if (!error) {
+                    console.log('URL "' + url + '" requested')
+                    const $ = cheerio.load(html);
+                    $(`tr[data-date=${date}] > td div[class=txt-slide]`).remove();
+                    const menuData = $(`tr[data-date=${date}] > td`)
+                        .text()
+                        .replace(/\n/g, ' ')
+                        .replace(/\s\s+/g, ' ')
+                        .replace(/News.*/, '')
+                        .trim()
+
+
+
+                    const menu = splitToMenus(menuData)
+
+
+                    const response = {
+                        date,
+                        menu
+                    }
+                    res.status(200).send(response);
+
+                    // Save to archive
+                    writeFileSync(filePath, JSON.stringify(response, null, 2))
+                } else {
+                    res.status(500).send({ error: error.message })
+                }
+            });
+        }
+    } else {
+        res.status(400).send({ error: "Day of the week must not be on the weekend" })
+    }
+});
+
+module.exports = app;
--- GIBZMensaAPI/index.js
+++ GIBZMensaAPI/index.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const request = require('request');
-const cheerio = require('cheerio');
-const { writeFileSync, statSync, readFileSync } = require('fs')
-const { join } = require('path')
-const moment = require('moment')
 const http = require('http');
+const express = require('express');
+const app = require('./app');
+const router = express.Router();
 
-const app = express();
 
-const menuTypes = [
-    "Tagesmenü",
-    "Vegimenü",
-    "Hit"
-]
 
-const archivePath = join(__dirname, '/archive')
+app.listen('8080');
+app.use('/', router);
+console.log('API is running at http://localhost:8080/api/v1/');
+
+
 
 const splitToMenus = (...menuStrings) => {
     for (const menuType of menuTypes) {
@@ -32,60 +27,7 @@ const splitToMenus = (...menuStrings) => {
     return menus
 }
 
-app.get('/api/v1/', (req, res) => {
-    const date = req.query.date;
-    const filePath = `${join(archivePath, date)}.json`
-    const url = 'https://zfv.ch/de/microsites/restaurant-treff/menuplan#' + date; //Date must be in format -> 2021-10-22
-    console.log('Date "' + date + '" requested')
-
-    // Decide if menu is already in the archive
-    const dayOfTheWeek = moment(date).format('d')
-    if (!["6", "0"].includes(dayOfTheWeek)) {
-        try {
-            statSync(filePath)
-            const cachedMenu = JSON.parse(readFileSync(filePath, 'utf-8'))
-            res.status(200).send(cachedMenu)
-        } catch (err) {
-            request(url, (error, response, html) => {
-                if (!error) {
-                    console.log('URL "' + url + '" requested')
-                    const $ = cheerio.load(html);
-                    $(`tr[data-date=${date}] > td div[class=txt-slide]`).remove();
-                    const menuData = $(`tr[data-date=${date}] > td`)
-                        .text()
-                        .replace(/\n/g, ' ')
-                        .replace(/\s\s+/g, ' ')
-                        .replace(/News.*/, '')
-                        .trim()
-
-                    const menu = splitToMenus(menuData)
-
-                    const response = {
-                        date,
-                        menu
-                    }
-                    res.status(200).send(response);
-
-                    // Save to archive
-                    writeFileSync(filePath, JSON.stringify(response, null, 2))
-                } else {
-                    res.status(500).send({ error: error.message })
-                }
-            });
-        }
-    } else {
-        res.status(400).send({ error: "Day of the week must not be on the weekend" })
-    }
-});
-
-app.listen('8080');
-console.log('API is running on http://localhost:8080');
-module.exports = app;
-
-
-
-
-const router = express.Router();
+module.exports = splitToMenus;
 
 router.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET');
@@ -101,5 +43,3 @@ router.get('/', (req, res) => {
 
     res.status(200).send(data);
 });
-
-app.use('/', router);
