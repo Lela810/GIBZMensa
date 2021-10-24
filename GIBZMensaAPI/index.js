4837e46315efc457fc3987b77c888f99f0e90c07
Lela810 <leandro.klaus03@gmail.com>
2021-10-24 02:57:35 +0200

    Update

--- GIBZMensaAPI/index.js
+++ GIBZMensaAPI/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mongoose = require("mongoose")
 
 
-mongoose.connect('mongodb://localhost/archive', { useNewUrlParser: true })
+mongoose.connect('mongodb://127.0.0.1/archive', { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
