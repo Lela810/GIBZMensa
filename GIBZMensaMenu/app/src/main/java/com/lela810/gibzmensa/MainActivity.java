f091e20a9ed093133c4c7e5f1cd19a71ef6df29d
Lela810 <leandro.klaus03@gmail.com>
2021-10-24 20:15:50 +0200

    Created android webview

--- GIBZMensaMenu/app/src/main/java/com/lela810/gibzmensa/MainActivity.java
+++ GIBZMensaMenu/app/src/main/java/com/lela810/gibzmensa/MainActivity.java
@@ -2,13 +2,34 @@ package com.lela810.gibzmensa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }});
+
+        WebSettings webSettings = webview.getSettings();
+        webview.getSettings().setAppCacheEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setWebChromeClient(new WebChromeClient());
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("file:///android_asset/index.html");
     }
 }
