f506912d266ff174da4192d8730121d6f9bd8e9b
Lela810 <leandro.klaus03@gmail.com>
2021-10-25 02:16:46 +0200

    Add Notification builder for future use

--- GIBZMensaMenu/app/src/main/java/com/lela810/gibzmensa/MainActivity.java
+++ GIBZMensaMenu/app/src/main/java/com/lela810/gibzmensa/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lela810.gibzmensa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +24,20 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel("GIBZ Mensa Menü", "GIBZ Mensa Menü", importance);
+                channel.setDescription("GIBZ Mensa Menü");
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+
 
         WebView webview = (WebView) findViewById(R.id.webview);
 
@@ -28,8 +50,24 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = webview.getSettings();
         webview.getSettings().setAppCacheEnabled(true);
         webview.getSettings().setDomStorageEnabled(true);
-        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebChromeClient(new WebChromeClient() {
+            //Other methods for your WebChromeClient here, if needed..
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "GIBZ Mensa Menü");
+                builder.setContentTitle("GIBZ Mensa Menü");
+                builder.setContentText(message);
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1,builder.build());
+                return super.onJsAlert(view, url, message, result);
+            }
+        });
         webSettings.setJavaScriptEnabled(true);
         webview.loadUrl("file:///android_asset/index.html");
+
     }
 }
