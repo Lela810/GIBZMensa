8afacfa10f73291a7459161a9bd3ca5a478b7794
Lela810 <leandro.klaus03@gmail.com>
2021-10-26 13:43:17 +0200

    Adjusted the Ratelimit

--- GIBZMensaAPI/index.js
+++ GIBZMensaAPI/index.js
@@ -6,8 +6,8 @@ const rateLimit = require('express-rate-limit')
 const app = require('./js/app');
 
 const limiter = rateLimit({
-        windowMs: 15 * 60 * 1000,
-        max: 100,
+        windowMs: 1 * 60 * 1000,
+        max: 10,
     })
     //TODO Cleanup Ratelimiter/ adjust rate limit
 
