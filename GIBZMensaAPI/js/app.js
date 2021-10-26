abe61c6705187732244ed88da91003bafe89f92f
Lela810 <71135175+Lela810@users.noreply.github.com>
2021-10-26 16:50:05 +0200

    Removed debugging TODOs

--- GIBZMensaAPI/js/app.js
+++ GIBZMensaAPI/js/app.js
@@ -46,12 +46,6 @@ app.use(cors({
 //TODO Add more comprehensive HTTP Codes 
 // assignees: lela810
 
-//TODO TTT
-// assignees: lela810
-
-//TODO TT
-// assignees: Lela810
-
 app.get('/api/v1/', limiter, async(req, res) => {
     const date = req.query.date;
     let dateNorm;
